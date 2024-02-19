c45f2b0a07c1693f15b449f0209578f7b265f2f3
Guilherme-Santt <guisant@icloud.com>
2024-02-19 13:21:28 -0300

    correção

--- scrpt.js
+++ scrpt.js
@@ -4,15 +4,9 @@ function procurar(){
    const ajax = new XMLHttpRequest();
     ajax.open('GET', 'https://viacep.com.br/ws/' + input + '/json/?callback=callback_name');
     ajax.send();
-
     ajax.onload = function(){
         document.getElementById("infoCEP").innerHTML = this.responseText;
-        // let obj = JSON.parse(this.responseText);
-        // let logradouro = obj.logradouro;
-        // let cidade = obj.localidade;
-        // let estado = obj.uf;
-        // document.getElementById("infoResumida").innerHTML = "Lougradouro: " + logradouro + "<br>Cidade: " + cidade + "<br>Estado: " + estado;
-
+        
     }
 }
 
